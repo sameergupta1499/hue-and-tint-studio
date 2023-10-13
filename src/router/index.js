a35ba88d4d55426467e29f7eeaa3069529a84660
sameer <sameergupta1499@gmail.com>
2023-10-13 17:53:08 +0530

    ADDED TEMPORARY ROUTES FOR ALL THE BRANDS

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,34 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage'
 import WorkPage from '../views/WorkPage'
 import WorkListPage from '../views/WorkListPage'
-import { BRANDS } from '@/assets/brandNameConst.js';
-
+import { data } from '@/assets/const.js';
+// Define a function to generate routes
+const getRoutes = () => {
+    const routes = [];
+  
+    // Iterate through data.work and create routes for each brand
+    data.work.forEach((item) => {
+      const route = {
+        path: item['page-url'],
+        // component: WorkListPage, //till al the brands are there
+        component: WorkPage,
+        props: {
+          brand: item.brand,
+        },
+      };
+      routes.push(route);
+    });
+  
+    return routes;
+  };
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes: [
         { path: '/', component: HomePage  },
         { path: '/work/', component: WorkPage },
-        { path: '/work/titan/', component: WorkListPage, props: { brand: BRANDS["titan"] }},
-        // { path: '/accessories/', component: ProductPage, props: { page: 'accessories' } },
+        { path: '/work/titan/', component: WorkListPage, props: { brand: "TITAN" }},
+        ...getRoutes(),
+
     ]
 })
 
